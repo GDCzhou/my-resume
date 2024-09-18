fd481d4eb71ede96ffc49309eb85d2db8c8890f1
GDCzhou <996078450@qq.com>
2024-09-18 10:06:51 +0800

    style

--- app/ui/Avatra.tsx
+++ app/ui/Avatra.tsx
@@ -3,18 +3,11 @@ import clsx from "clsx";
 
 export default function Avatra() {
   const img = process.env.NODE_ENV === 'development' ? '/avatra.jpg' : '/my-resume/avatra.jpg'
-  const classNames = clsx(
-    "w-[8rem] h-[10rem] bg-url(' + img + ')' bg-cover",
-  )
+
   return (
-    // <Image 
-    // src={img}
-    // alt="My Avatra"
-    // width={100}
-    // height={120}
- 
-    // />
-    <div className="w-[8rem] h-[10rem] bg-cover bg-[-53px] bg-no-repeat bg-[url('/avatra.jpg')] title">
+    <div className="w-[8rem] h-[10rem] bg-cover bg-[-53px] bg-no-repeat  title" 
+    style={{backgroundImage: `url(${img})`}}
+    >
 
     </div>
   )
