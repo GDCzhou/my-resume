68b49e90e5099cbd5c079fa9d5f21c7bb8bb706f
GDCzhou <996078450@qq.com>
2024-09-25 13:30:07 +0800

    更新: 教育经历专业名称修改

--- app/ui/Educat.tsx
+++ app/ui/Educat.tsx
@@ -5,7 +5,7 @@ const Education = () => {
     <div>
       <Title>教育经历</Title>
       <div  className="flex justify-between">
-      <h2 className="font-bold">湖南化工职业学院</h2> <span>大专</span> <span>机电一体化</span>
+      <h2 className="font-bold">湖南化工职业学院</h2> <span>大专</span> <span>计算机技术与科学</span>
 
       </div>
     </div>
