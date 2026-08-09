89792a05429a2fc4d284f15934899871684dca39
codezhou <1263283379@qq.com>
2026-08-09 20:37:24 +0800

    修改配置测试部署

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,30 @@
+// /** @type {import('next').NextConfig} */
+
+
+// const deployNextConfig = {
+//   output: 'export',
+//   // GitHub Pages 部署在 /my-resume 子路径；Cloudflare Pages（构建时自动设置 CF_PAGES=1）等平台走根路径
+//   basePath: process.env.CF_PAGES ? undefined : '/my-resume',
+//   images: {
+//     unoptimized: true
+//   }
+// }
+
+// const devNextConfig = {
+
+// }
+
+// const nextConfig = process.env.NODE_ENV === 'production' ? deployNextConfig : devNextConfig
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 
-
-const deployNextConfig = {
-  output: 'export',
-  // GitHub Pages 部署在 /my-resume 子路径；Cloudflare Pages（构建时自动设置 CF_PAGES=1）等平台走根路径
-  basePath: process.env.CF_PAGES ? undefined : '/my-resume',
+const nextConfig = {
   images: {
-    unoptimized: true
-  }
-}
-
-const devNextConfig = {
-  
-}
-
-const nextConfig = process.env.NODE_ENV === 'production' ? deployNextConfig : devNextConfig
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
