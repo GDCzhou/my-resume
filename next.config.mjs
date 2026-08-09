e34cc9d85f26062a62551f5ad231e1a5ffd9d97f
codezhou <1263283379@qq.com>
2026-08-09 21:18:36 +0800

    feat: 纯静态

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,21 @@
-// /** @type {import('next').NextConfig} */
-
-
-// const deployNextConfig = {
-//   output: 'export',
-//   // GitHub Pages 部署在 /my-resume 子路径；Cloudflare Pages（构建时自动设置 CF_PAGES=1）等平台走根路径
-//   basePath: process.env.CF_PAGES ? undefined : '/my-resume',
-//   images: {
-//     unoptimized: true
-//   }
-// }
-
-// const devNextConfig = {
-
-// }
-
-// const nextConfig = process.env.NODE_ENV === 'production' ? deployNextConfig : devNextConfig
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
+
+const deployNextConfig = {
+  output: 'export',
+  // GitHub Pages 部署在 /my-resume 子路径；Cloudflare Pages（构建时自动设置 CF_PAGES=1）等平台走根路径
+  basePath: process.env.CF_PAGES ? undefined : '/my-resume',
   images: {
-    unoptimized: true,
-  },
-};
+    unoptimized: true
+  }
+}
+
+const devNextConfig = {
+
+}
+
+const nextConfig = process.env.NODE_ENV === 'production' ? deployNextConfig : devNextConfig
 
 export default nextConfig;
+
+
